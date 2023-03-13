64d7b77e0f34d2c2963b3c0f770576645c9fde3d
shanzhao <shanzhao@nudt.edu.cn>
2023-03-13 18:23:02 +0800

    add lag

--- evaluation/decompose.py
+++ evaluation/decompose.py
@@ -8,6 +8,18 @@ data_path = os.path.join(script_path, '../output/'+use_data+'/')
 true_state_seq_path = os.path.join(script_path, '../data/synthetic_data/state_seq_'+use_data+'/')
 data_path = os.path.join(script_path, '../output/'+use_data+'/')
 
+def add_lag(X, Y, lag):
+    if lag>0:
+        seq1 = X[lag:]
+        seq2 = Y[:-lag]
+    elif lag<0:
+        seq1 = X[:lag]
+        seq2 = Y[-lag:]
+    else:
+        seq1 = X
+        seq2 = Y
+    return seq1, seq2
+
 def decompose_state_seq(X):
     state_set = set(X)
     # return state_set
@@ -28,16 +40,31 @@ def score(X,Y):
     # return np.log(1)
     # scores = p_xy*np.log(p_y_given_x/p_x)
     scores = p_xy*p_y_given_x/p_x
-    print(p_x, p_xy, p_y_given_x, scores)
+    # print(p_x, p_xy, p_y_given_x, scores)
     return scores
+
+def partial_state_corr(X,Y):
+    listX = decompose_state_seq(X)
+    listY = decompose_state_seq(Y)
+    score_matrix = np.zeros((len(listX),len(listY)))
+    for i in range(len(listX)):
+        for j in range(len(listY)):
+            sssX = listX[i]
+            sssY = listY[j]
+            Jscore = score(sssX, sssY)
+            score_matrix[i,j] = Jscore
+    return score_matrix
 
 score(np.array([0,0,0,1,1,0,0]), np.array([0,0,0,1,1,0,0]))
 
-decomposed_state_seq_list = []
+state_seq_list = []
 for i in range(2):
     state_seq = np.load(os.path.join(data_path, 'state_seq/test'+str(i)+'.npy'))
-    decomposed_state_seq_list.append(decompose_state_seq(state_seq))
+    state_seq_list.append(state_seq)
 
-for sseq1 in decomposed_state_seq_list[0]:
-    for sseq2 in decomposed_state_seq_list[1]:
-        score(sseq1, sseq2)
+matrix = partial_state_corr(state_seq_list[0], state_seq_list[1])
+print(np.round(matrix, 2))
+
+import matplotlib.pyplot as plt
+plt.imshow(matrix)
+plt.savefig('partial.png')
