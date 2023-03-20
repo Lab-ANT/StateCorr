36774198d4fd145d07c577a11154cea98fd57f9f
Chengyu <chengyu@nudt.edu.cn>
2023-03-21 06:45:06 +0800

    update hanning

--- evaluation/hanning.py
+++ evaluation/hanning.py
@@ -2,21 +2,35 @@ import math
 import numpy as np
 import torch
 
-x = np.arange(0,20*math.pi,0.001)
-v = np.cos(x)
-length = len(x)
-print(v)
 
-# print(length)
-data = torch.ones(16,4,length)
+x = np.arange(0,20*math.pi,0.001)
+v = np.cos(x)+1
+v = np.stack([v,v,v,v])
+v = np.stack([v,v,v,v,v,v,v,v])
+data = torch.tensor(v)
+print(data.size())
 
 def hanning_tensor(X):
     length = X.size(2)
-    print(length)
+    # print(length)
     weight = (1-np.cos(2*math.pi*np.arange(length)/length))/2
-    weight
+    weight = torch.tensor(weight)
+    return weight*data
 
-hanning_tensor(data)
+new_data = hanning_tensor(data)
+
+import matplotlib.pyplot as plt
+plt.plot(new_data[1,1,:])
+plt.show()
+
+
+# hanning_tensor(data)
+
+# x = np.arange(0,20*math.pi,0.001)
+# v = np.cos(x)+1
+# length = len(x)
+# print(v)
+
 # def hanning(X):
 #     length = len(X)
 #     return (1-np.cos(2*math.pi*np.arange(length)/length))/2
