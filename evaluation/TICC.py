3c7cd315060bcb2979ce6a8671e4ab88b4bdc1d3
shanzhao <shanzhao@nudt.edu.cn>
2023-03-29 16:48:52 +0800

    add HDP-HSMM

--- evaluation/TICC.py
+++ evaluation/TICC.py
@@ -10,8 +10,7 @@ from Time2State.time2state import Time2State
 from Time2State.adapers import *
 from Time2State.clustering import *
 from Time2State.default_params import *
-
-# use_data = 'dataset2'
+from Baselines.hdphsmm import *
 
 script_path = os.path.dirname(__file__)
 
@@ -62,7 +61,22 @@ def use_TICC(use_data):
         prediction = prediction.astype(int)
         np.save(output_path+file_name[:-4]+'.npy', prediction)
 
+def use_HDPHSMM(use_data):
+    data_path = os.path.join(script_path, '../data/synthetic_data/'+use_data)
+    file_list = os.listdir(data_path)
+    output_path = os.path.join(script_path, '../output/output_HDP-HSMM/'+use_data+'/state_seq/')
+
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    for file_name in tqdm.tqdm(file_list):
+        data = np.load(os.path.join(data_path, file_name))
+        prediction = HDP_HSMM(1e4, 20, n_iter=20).fit(data)
+        np.save(os.path.join(script_path, '../output/output_HDP-HSMM/'+use_data+'/state_seq/'+file_name[:-4]+'.npy'), prediction)
+
+
 for i in range(1,6):
     dataset_name = 'dataset'+str(i)
-    use_StateCorr(dataset_name)
-    use_TICC(dataset_name)
+    use_HDPHSMM(dataset_name)
+    # use_StateCorr(dataset_name)
+    # use_TICC(dataset_name)
