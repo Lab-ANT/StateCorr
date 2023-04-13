45601ac8fabe3de0f07b9dd144423c324cb59444
chengyu <chengyu@nudt.edu.cn>
2023-04-13 13:59:25 +0000

    add ClaSP

--- evaluation/evaluate_RQ3.py
+++ evaluation/evaluate_RQ3.py
@@ -7,7 +7,7 @@ from TSpy.eval import ARI
 num_in_group = 5
 num = 5
 # method_list = ['HDP-HSMM', 'ClaSP', 'AutoPlait', 'StateCorr', 'TICC']
-method_list = ['StateCorr']
+method_list = ['StateCorr','ClaSP']
 script_path = os.path.dirname(__file__)
 
 def find_match(score_matrix):
