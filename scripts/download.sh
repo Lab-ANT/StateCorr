3165f5f63e5b737e88dcb383b2198267e2c6182d
chengyu <chengyu@nudt.edu.cn>
2023-04-13 10:57:14 +0000

    update script

--- scripts/download.sh
+++ scripts/download.sh
@@ -1,4 +1,8 @@
 # bash
 
-wget -c --timestamping  https://sipi.usc.edu/had/USC-HAD.zip -O data/USC-HAD.zip
-unzip -o data/USC-HAD.zip -d data/
+# wget -c --timestamping  https://sipi.usc.edu/had/USC-HAD.zip -O data/USC-HAD.zip
+# unzip -o data/USC-HAD.zip -d data/
+
+# git clone git@github.com:andreas-bulling/ActRecTut
+# mv ActRecTut/Data/ data/ActRecTut
+# rm -rf ActRecTut
