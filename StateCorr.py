2245a871a6ab9f85cc7fc4fcbf4dfeb23714ff7d
Chengyu <chengyu@nudt.edu.cn>
2023-06-06 21:09:01 +0800

    update

--- StateCorr.py
+++ StateCorr.py
@@ -1,11 +1,12 @@
-from TSpy.utils import normalize
 import sys
+import tqdm
 sys.path.append('./')
 from Time2State.time2state import Time2State
 from Time2State.adapers import *
 from Time2State.clustering import *
 from Time2State.default_params import *
-import tqdm
+from TSpy.utils import normalize
+from TSpy.corr import state_correlation, lagged_state_correlation
 
 class StateCorr():
     def __init__(self) -> None:
@@ -23,17 +24,37 @@ class StateCorr():
         num_channels = X.shape[2]
 
         params_LSE['in_channels'] = num_channels
-        params_LSE['out_channels'] = 1
+        params_LSE['out_channels'] = 2
         params_LSE['nb_steps'] = 40
         params_LSE['win_size'] = win_size
-        # params_LSE['win_type'] = 'hanning'
+        params_LSE['win_type'] = 'hanning'
 
         state_seq_list = []
         for i in tqdm.tqdm(range(num_ts)):
             t2s = Time2State(win_size, step, CausalConv_LSE_Adaper(params_LSE), DPGMM(None)).fit(normalize(X[i,:].reshape(-1,1)), win_size, step)
             state_seq_list.append(t2s.state_seq)
-        state_seq_list = np.array(state_seq_list)
-        return state_seq_list
+        self.__state_seq_list = np.array(state_seq_list)
+        return self
+
+    def calculate_matrix(self):
+        self.__corr_matrix = state_correlation(self.__state_seq_list)
+        # self.__corr_matrix, self.__lag_matrix = lagged_state_correlation(self.__state_seq_list)
+        return self
+
+    def load_state_seq_list(self, X):
+        self.__state_seq_list = X
+
+    @property
+    def state_seq_list(self):
+        return self.__state_seq_list
+    
+    @property
+    def corr_matrix(self):
+        return self.__corr_matrix
+
+    @property
+    def lag_matrix(self):
+        return self.__lag_matrix
 
     def fit(self):
         pass
--- case_studies/WADI.py
+++ case_studies/WADI.py
@@ -1,13 +1,18 @@
 import os
 import matplotlib.pyplot as plt
 import os
-import pandas as pd
 import numpy as np
 import sys
 sys.path.append('./')
 from StateCorr import StateCorr
 from TSpy.utils import z_normalize
 
+USE_COMPUTED_STATE_SEQ = False
+USE_COMPUTED_MATRIX = False
+
+"""
+Load data
+"""
 script_path = os.path.dirname(__file__)
 data_path = os.path.join(script_path, '../data/2019.npy')
 
@@ -23,53 +28,83 @@ for i in range(data.shape[1]):
         unused_col.append(i)
 
 data = np.delete(data, unused_col, axis=1)
-
 data = np.expand_dims(data[::20].T, axis=2)
 print(data.shape)
 
+data = data[:20]
 sc = StateCorr()
-state_seq_list = sc.fit_predict(data[:6], 128, 20)
-np.save('state_seq.npy', state_seq_list)
 
-state_seq_list = np.load('state_seq.npy')
+"""
+Use StateCorr
+"""
+if USE_COMPUTED_STATE_SEQ:
+    state_seq_list = np.load('state_seq.npy')
+    sc.load_state_seq_list(state_seq_list)
+else:
+    state_seq_list = sc.fit_predict(data, 256, 50).state_seq_list
+    np.save('state_seq.npy', state_seq_list)
 
-import matplotlib.pyplot as plt
+if USE_COMPUTED_MATRIX:
+    corr_matrix = np.load('corr_matrix.npy')
+else:
+    corr_matrix = sc.calculate_matrix().corr_matrix
+    np.save('corr_matrix.npy', corr_matrix)
 
-plt.style.use('classic')
-fig, ax = plt.subplots(nrows=6, sharex=True)
+"""
+Show Results
+"""
+def find_top_k(id, k, matrix, state_seq_list):
+    col = matrix[:,id]
+    idx = np.argsort(-col)
+    print(idx)
 
-for i in range(6):
-    ax[i].plot(z_normalize(data[i]))
-    state_seq = state_seq_list[i].reshape(1,-1)
-    state_seq = np.concatenate([state_seq, state_seq])
-    ax[i].imshow(state_seq, aspect='auto', cmap='tab20c', interpolation='nearest', alpha=0.5, origin='lower')
+    plt.style.use('classic')
+    fig, ax = plt.subplots(nrows=k, sharex=True, figsize=(8,4.5))
+    for i in range(k):
+        print(idx[i])
+        data_ = z_normalize(data[idx[i]])
 
-plt.tight_layout()
-plt.show()
+        state_seq = state_seq_list[idx[i]].reshape(1,-1)
+        state_seq = np.concatenate([state_seq, state_seq])
+        ax[i].imshow(state_seq, aspect='auto', cmap='tab20c', interpolation='nearest', alpha=0.6, origin='lower', vmax=5, vmin=0)
+        ax[i].plot(data_, lw=0.5)
+        ax[i].set_ylim([-0.1,1.1])
 
+        if i == 0:
+            ax[i].set_title('Indicator %d'%(i+1), loc='left', y=0.55, x=0.02,
+                fontsize='medium')
+        elif i == 1:
+            ax[i].set_title('%d-st state-correlated indicator'%(i), loc='left', y=0.55, x=0.02,
+                fontsize='medium')
+        elif i == 2:
+            ax[i].set_title('%d-nd state-correlated indicator'%(i), loc='left', y=0.55, x=0.02,
+                fontsize='medium')
+        elif i == 3:
+            ax[i].set_title('%d-rd state-correlated indicator'%(i), loc='left', y=0.55, x=0.02,
+                fontsize='medium')
+        else:
+            ax[i].set_title('%d-th state-correlated indicator'%(i), loc='left', y=0.55, x=0.02,
+                fontsize='medium')
+    
+    plt.tight_layout()
+    plt.subplots_adjust(hspace=0.2)
+    plt.show()
+    print(idx[:k])
 
+find_top_k(15, 6, corr_matrix, state_seq_list)
 
+# find_top_k(18, 5)
 
+# import matplotlib.pyplot as plt
 
+# plt.style.use('classic')
+# fig, ax = plt.subplots(nrows=20, sharex=True)
 
+# for i in range(20):
+#     ax[i].plot(z_normalize(data[i]))
+#     state_seq = state_seq_list[i].reshape(1,-1)
+#     state_seq = np.concatenate([state_seq, state_seq])
+#     ax[i].imshow(state_seq, aspect='auto', cmap='tab20c', interpolation='nearest', alpha=0.5, origin='lower')
 
-
-
-
-
-# df = pd.DataFrame(data[:500000])
-
-# fig, ax = plt.subplots(nrows=10, sharex=True)
-# for i in range(10):
-#     ax[i].plot(data[::20, i+10])
-# plt.show()
-
-# matrix = df.corr('pearson').to_numpy()
-# idx = np.isnan(matrix)
-# print(idx)
-# print(matrix.shape)
-# matrix,_,_ = cluster_corr(matrix)
-# # plt.imshow(matrix, cmap='gray')
-# plt.imshow(matrix)
-# plt.colorbar()
+# plt.tight_layout()
 # plt.show()
